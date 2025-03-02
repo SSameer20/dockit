a05c6f154e1ff598212d16e72d99bb16e4eebf03
Sameer Shaik <112153702+SSameer20@users.noreply.github.com>
2025-03-02 18:26:51 +0530

    Delete server/dist directory

--- server/dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRouter = void 0;
-const express_1 = require("express");
-const userController_1 = require("../controller/userController");
-exports.userRouter = (0, express_1.Router)();
-exports.userRouter.post("/create", userController_1.createUser);
-exports.userRouter.get("/all", userController_1.GetAllUser);
